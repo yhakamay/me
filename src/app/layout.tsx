fcf29c4faf86592bbf1db83067471da54e3e74bf
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-01-29 22:12:55 +0900

    setup Chakra UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Provider from "./provider";
 
 export default function RootLayout({
   children,
@@ -8,7 +9,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
