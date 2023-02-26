29edbc04cec20f70bd8e3ae082fa4c5523c7b526
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-02-26 21:20:41 +0900

    update layout.tsx
    using @chakra-ui/next-js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
-import Provider from "./provider";
+"use client";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "./components/organisms/header";
+import Footer from "./components/organisms/footer";
 
 export default function RootLayout({
   children,
@@ -9,7 +14,13 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body>
-        <Provider>{children}</Provider>
+        <CacheProvider>
+          <ChakraProvider>
+            <Header />
+            {children}
+            <Footer />
+          </ChakraProvider>
+        </CacheProvider>
       </body>
     </html>
   );
