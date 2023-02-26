847316bde8518ba4ce13cb19a6819c072ce75b47
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-02-26 21:21:20 +0900

    add some components
    header, footer and main

---
+++ src/app/components/organisms/main.tsx
@@ -0,0 +1,14 @@
+import { Container } from "@chakra-ui/react";
+
+export default function Main({ children }: { children: React.ReactNode }) {
+  return (
+    <Container
+      as="main"
+      maxW="container.lg"
+      my="4"
+      minH="calc(100vh - 115px - 2rem)"
+    >
+      {children}
+    </Container>
+  );
+}
