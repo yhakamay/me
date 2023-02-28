e6dd697517bb09e732dcabc7f7437a025cf4068a
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-02-28 22:42:14 +0900

    new logo ✌️

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -1,13 +1,17 @@
 import { Link } from "@chakra-ui/next-js";
-import { Heading, Box, Container } from "@chakra-ui/react";
+import { Heading, Box, Container, HStack } from "@chakra-ui/react";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <Box as="header">
       <Container py={4}>
-        <Heading size="lg">
-          <Link href="/">yhakamay</Link>
-        </Heading>
+        <HStack>
+          <Image src="/logo.png" width={24} height={24} alt={""} />
+          <Heading size="md">
+            <Link href="/">yhakamay</Link>
+          </Heading>
+        </HStack>
       </Container>
     </Box>
   );
