4cfa5572cf940a140f838c2885d0dc73c0ff9e1f
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-02-28 20:17:14 +0900

    update the layout

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -1,16 +1,14 @@
 import { Link } from "@chakra-ui/next-js";
-import { Flex, Heading, Box } from "@chakra-ui/react";
+import { Heading, Box, Container } from "@chakra-ui/react";
 
 export default function Header() {
   return (
     <Box as="header">
-      <Flex minH={"60px"} py={{ base: 2 }} px={{ base: 4 }} align="center">
-        <Flex flex={1} justify="space-between" maxW="5xl" mx="auto">
-          <Heading size="lg">
-            <Link href="/">yhakamay</Link>
-          </Heading>
-        </Flex>
-      </Flex>
+      <Container py={4}>
+        <Heading size="lg">
+          <Link href="/">yhakamay</Link>
+        </Heading>
+      </Container>
     </Box>
   );
 }
--- src/components/organisms/main.tsx
+++ src/components/organisms/main.tsx
@@ -1,14 +1,5 @@
 import { Container } from "@chakra-ui/react";
 
 export default function Main({ children }: { children: React.ReactNode }) {
-  return (
-    <Container
-      as="main"
-      maxW="container.lg"
-      my="4"
-      minH="calc(100vh - 115px - 2rem)"
-    >
-      {children}
-    </Container>
-  );
+  return <Container as="main">{children}</Container>;
 }
