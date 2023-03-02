4342d5d93024c3e0b7b077f87438ab9b0812ecdc
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-03-03 01:17:23 +0900

    tidy up metatags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,22 @@ import { Inter } from "next/font/google";
 
 export const metadata = {
   title: "yhakamay",
-  description: "yhakamay.me",
+  description:
+    "yhakamay is ex-42 student, technical consultant, and Next.js lover.",
+  og: {
+    title: "yhakamay",
+    type: "website",
+    url: "https://yhakamay.me",
+    image: "https://yhakamay.me/logo.png",
+    description:
+      "yhakamay is ex-42 student, technical consultant, and Next.js lover.",
+    site_name: "yhakamay",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@yhakamay",
+    creator: "@yhakamay",
+  },
 };
 
 const inter = Inter({
