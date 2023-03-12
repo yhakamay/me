ee44f8bec67142ce51af79c2af9c2562d61c921b
Yusuke <yhakamaya@adobe.com>
2023-03-12 16:42:41 +0900

    animate profile image

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 import FadeInSection from "./fade_in_section";
@@ -9,7 +10,22 @@ export default function Profile() {
   return (
     <>
       <FadeInSection className={styles.profile}>
-        <Image src="/yhakamay.png" width={60} height={60} alt={"yhakamay"} />
+        <motion.div
+          initial={{ opacity: 0, scale: 0.8 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true }}
+          drag
+          dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+          dragElastic={0.02}
+          transition={{
+            type: "spring",
+            duration: 2,
+            stiffness: 100,
+            delay: 0.2,
+          }}
+        >
+          <Image src="/yhakamay.png" width={60} height={60} alt={"yhakamay"} />
+        </motion.div>
         <p>
           yhakamay is ex-42 student, technical consultant, and Next.js lover.
         </p>
