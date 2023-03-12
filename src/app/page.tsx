8e92e14bcde69ebb8567dae299dfea6c81e6a5f1
Yusuke <yhakamaya@adobe.com>
2023-03-13 00:22:08 +0900

    add languages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Contacts from "@/components/contacts";
+import Languages from "@/components/languages";
 import Profile from "@/components/profile";
 import Repos from "@/components/repos";
 import { Repo } from "@/types/repo";
@@ -7,10 +8,12 @@ import styles from "./page.module.scss";
 
 export default async function Home() {
   const repos = (await getRepos()) satisfies Repo[];
+  const languages = (await getLanguages()) satisfies [string, number][];
 
   return (
     <main className={styles.main}>
       <Profile />
+      <Languages languages={languages} />
       <Repos repos={repos} />
       <Contacts />
     </main>
@@ -32,4 +35,19 @@ async function getRepos() {
   }
 
   return res.json();
+}
+
+async function getLanguages(): Promise<[string, number][]> {
+  const repos = (await getRepos()) satisfies Repo[];
+
+  const languages = new Map<string, number>();
+
+  for (const repo of repos) {
+    if (repo.language) {
+      const count = languages.get(repo.language) || 0;
+      languages.set(repo.language, count + 1);
+    }
+  }
+
+  return Array.from(languages.entries()).sort((a, b) => b[1] - a[1]);
 }
