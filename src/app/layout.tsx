c78bbf3971ed0f01db4ad9902afda17242fb69e1
Yusuke <yhakamaya@adobe.com>
2023-05-27 21:46:24 +0900

    introduce dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className={`${inter.className}`}>
+      <body className={`${inter.className} bg-white dark:bg-slate-950`}>
         <Header />
         {children}
         <Footer />
