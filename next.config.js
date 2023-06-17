39018af641c95a13dc220b313456722a122677c6
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-06-17 15:45:01 +0900

    configure image-loader.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'custom',
+    loaderFile: './src/utils/image-loader.js'
+  }
 };
 
 module.exports = nextConfig;
