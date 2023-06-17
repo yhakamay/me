96bf4e1d3262204167b29f442c47ae952fb76b5f
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2023-06-17 15:50:37 +0900

    Revert "configure image-loader.js"
    This reverts commit 39018af641c95a13dc220b313456722a122677c6.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: 'custom',
-    loaderFile: './src/utils/image-loader.js'
-  }
 };
 
 module.exports = nextConfig;
