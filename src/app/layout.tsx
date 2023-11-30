a2f8e9fb1441394e438d4c2eb45b874adc2ee121
Yusuke <yhakamaya@adobe.com>
2023-11-30 17:48:42 +0900

    update layout.tsx
    replace Inter with Geist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import { GeistMono } from "geist/font/mono";
+import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-
-import { Inter } from "next/font/google";
 
 import Footer from "@/components/organisms/footer";
 
@@ -26,10 +26,6 @@ export const metadata = {
   },
 };
 
-const inter = Inter({
-  subsets: ["latin"],
-});
-
 export default function RootLayout({
   children,
 }: {
@@ -38,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className={inter.className}>
+      <body className={`${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
         <Footer />
       </body>
